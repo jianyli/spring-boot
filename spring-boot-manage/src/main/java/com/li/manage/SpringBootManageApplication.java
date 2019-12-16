71737a0c9f0872e19d1b8c0e77aca0af09c8585d
jianyli <172961950@qq.com>
2019-12-16 12:15:30 +0800

    上传项目架构

---
+++ spring-boot-manage/src/main/java/com/li/manage/SpringBootManageApplication.java
@@ -0,0 +1,13 @@
+package com.li.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootManageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootManageApplication.class, args);
+    }
+
+}
---
+++ spring-boot-support/src/test/java/com/li/support/SpringBootSupportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.li.support;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSupportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
